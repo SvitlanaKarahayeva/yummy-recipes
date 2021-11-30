8831e5e5d78c301466ec50701e9bd9e244a0f681
Svitlana Karahayevae <svitlana.karahayeva@gmail.com>
2021-11-30 07:57:25 -0500

    Adding get all posts with query fetch, tested on Postman, works well for user search, need to come back to categories

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const recipesCtrl = require('../controllers/recipes')
 
-// /* GET all recipes */
-// router.get('/', recipesCtrl.index)
+/* GET all recipes */
+router.get('/', recipesCtrl.index)
 // /* GET one recipe */
 // router.get('/', recipesCtrl.index)
 
