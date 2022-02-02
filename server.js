8679f810f3364d2e1f3fba2bf824a390847b9077
Svitlana Karahayevae <svitlana.karahayeva@gmail.com>
2022-02-02 02:44:48 -0500

    adding changes

--- server.js
+++ server.js
@@ -31,7 +31,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 
 // The following "catch all" route (note the *)is necessary
-// for a SPA's client-side routing to properly work
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
